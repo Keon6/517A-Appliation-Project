29ba2a08aa6f915942e9b0a12d5208379b5e1e61
kevin <kevinkim1207@gmail.com>
2019-03-28 14:13:56 -0500

    local GP 99% done

--- Milestone2/localGP.py
+++ Milestone2/localGP.py
@@ -5,10 +5,16 @@ from sklearn.gaussian_process.kernels import (Matern, RationalQuadratic,
                                               ExpSineSquared)
 from sklearn.metrics import roc_auc_score
 from sklearn.neighbors import BallTree
+import numpy as np
 
 
 class LocalGaussianProcessClassifier:
     def __init__(self, kernel_hyperparams, kernel_type="Matern", k=100):
+        """
+        :param kernel_hyperparams: list of hyperparameters to input
+        :param kernel_type: str for kernel type
+        :param k: value for k-NN
+        """
         self.kernel_hyperparams = kernel_hyperparams
         self.kernel_type = kernel_type
         self.k = k
@@ -28,10 +34,17 @@ class LocalGaussianProcessClassifier:
         }
 
     def predict(self, X_tr, X_te, Y_tr):
+        """
+        There is no "Model fitting" phase for local GP
+        :param X_tr: Training Data (n x d)
+        :param X_te: Test Data (m x d)
+        :param Y_tr: Training Labels (n x 1)
+        :return: prediction labels (n x 1)
+        """
         # runn k-NN first
         tree = BallTree(X_tr, leaf_size=2)
         dist, ind = tree.query(X_te, k=self.k)
-        # GP Calssification
+        # GP Classification
         model = GaussianProcessClassifier(
             kernel=self.kernel_map[self.kernel_type],
             random_state=0, n_jobs=-1
@@ -50,33 +63,139 @@ class LocalGaussianProcessClassifier:
                 Y_prediction.append(model.predict(x_new)[0])
         return np.asarray(Y_prediction).reshape(-1, 1)
 
-    def score(self, X_tr, Y_tr):
-        """ Returns AUC for training data
+    def score(self, X_tr, Y_tr, score_criteria):
+        """
+        score for training data
+        :param X_tr:
+        :param Y_tr:
+        :param score_criteria: criteria for scoring ie) roc_auc_score, ...
+        :return: Returns score for training data (float)
         """
         Y_pred = self.predict(X_tr, X_tr, Y_tr)
-        return roc_auc_score(Y_tr, Y_pred)
+        return score_criteria(Y_tr, Y_pred)
 
-    def test_score(self, X_tr, Y_tr, X_te, Y_te):
-        """ Returns AUC for test data
+    def test_score(self, X_tr, Y_tr, X_te, Y_te, score_criteria):
+        """
+        :param X_tr:
+        :param Y_tr:
+        :param X_te:
+        :param Y_te:
+        :param score_criteria: criteria for scoring ie) roc_auc_score, ...
+        :return: returns test score
         """
         Y_pred = self.predict(X_tr, X_te, Y_tr)
-        return roc_auc_score(Y_te, Y_pred)
+        return score_criteria(Y_te, Y_pred)
 
 
 class LGPC_CV:
     """ CV for Local Gaussian Process Calssifier
     """
 
-    def __init__(self, model, k=5):
+    def __init__(self, model, score_criteria=roc_auc_score, n_splits=5, split_method=StratifiedKFold):
         """
-        k = nubmer of splits (for k-fold cv and shuffle& split CV)
-        model = LGPC model
+        :param model: model object for LGPC
+        :param n_splits: number of splits for CV
+        :param score_criteria: object for scoring criteria ie) roc_auc_score
+        :param split_method:
         """
-        self.k = k
+        self.n_splits = n_splits
         self.model = model
+        self.score_criteria = score_criteria
+        self.split_method = split_method
 
-    def run_cv(X, Y, split_method):
+    def run_cv(self, X, Y):
         """
-        split_method = iterator object for data splits. Choose among
-            (KFold, StratifiedKFold, ShuffleSplit, StratifiedShuffleSplit)
+
+        :param X:
+        :param Y:
+        :param split_method: iterator object for data splits.
+        Choose among
+        (KFold, StratifiedKFold, ShuffleSplit, StratifiedShuffleSplit)
+        :return: array of scores
         """
+        cv_scores = []
+        splits = self.split_method(self.n_splits)
+        for tv_index, val_index in splits.split(X):
+            X_tv, Y_tv = X[tv_index], Y[tv_index]
+            X_val, Y_val = X[val_index], Y[val_index]
+            cv_scores.append(self.model.test_score(X_tv, Y_tv, X_val, Y_val, self.score_criteria))
+        return cv_scores
+
+
+class LGPC_GridSearchCV:
+    """
+    Run GridSearch with CV for LPGC
+    """
+    def __init__(self, hyp_test, param_grid, score_criteria=roc_auc_score, n_splits=5, split_method=StratifiedKFold):
+        """
+        :param hyp_test: str - type of hypothesis testing to do (ie. score, t, ...)
+        :param param_grid: dict() for parameters
+        :param score_criteria: object for scoring criteria ie) roc_auc_score
+        :param n_splits: numbero f splits for CV
+        :param split_method: onject for splitting data. Chose among
+        (KFold, StratifiedKFold, ShuffleSplit, StratifiedShuffleSplit)
+        """
+        self.hyp_test_type = hyp_test
+        self.param_grid = param_grid
+        # ex)
+        # TODO: how to exactly match kernel type with kernel hyperparameters?
+        # param_grid = {
+        #     "kernel_hyperparams": [[], []],
+        #     "kernel_type": ["Matern", "RationalQuadratic", "ExpSineSquared"],
+        #     "k": [50, 100, 150, 200]
+        # }
+        self.score_criteria = score_criteria
+        self.n_splits = n_splits
+        self.split_method = split_method
+
+    def run(self, X, Y):
+        scores = dict()
+
+        # Run CV on all combinations & save scores
+        for kernel_type in self.param_grid["kernel_type"]:
+            for kernel_hyperparams in self.param_grid["kernel_hyperparams"]:
+                for k in self.param_grid["k"]:
+                    model = LocalGaussianProcessClassifier(kernel_hyperparams=kernel_hyperparams,
+                                                           kernel_type=kernel_type, k=k)
+
+                    scores[(kernel_type, kernel_hyperparams, k)] = \
+                        LGPC_CV(model=model, score_criteria=self.score_criteria,
+                                n_splits=self.n_splits, split_method=self.split_method).run_cv(X, Y)
+
+        # TODO: Write selection method for optimal hyperparameter
+        # do sth with scores to achieve the above
+        optimal_hyperparams = {
+            "kernel_hyperparams": self.param_grid["kernel_hyperparams"][0],
+            "kernel_type": self.param_grid["kernel_type"][0],
+            "k": self.param_grid["k"][0]
+        }
+        return optimal_hyperparams
+
+
+# example
+n = 100  # number of data points
+d = 5  # dimensions
+X = 100*np.random.random((n, d))-50
+Y = np.sign(np.sign(2*np.random.random((n, 1))-1)+0.1)  # y in {-1, +1}
+
+# Model Building and Predicting
+X_test = 100*np.random.random((70, d))-50  # test data
+model = LocalGaussianProcessClassifier(kernel_hyperparams=[1, (0.01, 5), 1.5], kernel_type="Matern", k=25)
+Y_test_predictions = model.predict(X_tr=X, Y_tr=Y, X_te=X_test)
+predictions = model.predict(X_tr=X, Y_tr=Y, X_te=X_test)
+training_score = model.score(X_tr=X, Y_tr=Y, score_criteria=roc_auc_score)
+
+# CV
+CV = LGPC_CV(model=model, score_criteria=roc_auc_score, n_splits=5, split_method=StratifiedKFold)
+cv_scores = CV.run_cv(X=X, Y=Y)
+
+# GridSearch CV
+param_grid = {
+    "kernel_hyperparams": [[1.0, (0.001, 10), 0.5], [1.0, (0.01, 10), 2.5], [2.0, (0.01, 5), float("inf")]],
+    "kernel_type": ["Matern"],
+    "k": [100, 150]
+}
+grid_search_cv = LGPC_GridSearchCV(hyp_test="t", param_grid=param_grid,
+                                   score_criteria=roc_auc_score, n_splits=5, split_method=StratifiedKFold)
+optimal_hyperparameters = grid_search_cv.run(X, Y)
+
